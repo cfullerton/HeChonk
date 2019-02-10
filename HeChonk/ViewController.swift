6417765e1641220896ffc00026108f0be814dcfe
Conner Fullerton <connerfullerton@conners-mbp.attlocal.net>
2019-02-10 00:30:20 -0600

    app store submitted

--- HeChonk/ViewController.swift
+++ HeChonk/ViewController.swift
@@ -7,14 +7,136 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,AVCapturePhotoCaptureDelegate {
+    @IBOutlet weak var chonkLevel: UILabel!
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+        guard let imageData = photo.fileDataRepresentation()
+            else { return }
+        
+        let image = UIImage(data: imageData)
+        let resizedImaged = resizeImage(image: image!, targetSize: CGSize.init(width: 355, height: 500))
+        let imageMeta = resizedImaged.jpegData(compressionQuality:1.0)!
+        let base64 = imageMeta.base64EncodedData(options: .lineLength64Characters)
+        let url = URL(string: "https://9vwg7hlurh.execute-api.us-east-1.amazonaws.com/prod")
+        var request = URLRequest(url: url!)
+        request.setValue("image/jpeg", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = base64
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                //create json object from data
+                if let chonkText = String(data: data, encoding: String.Encoding.utf8) {
+                    print(chonkText)
+                    DispatchQueue.main.async {
+                        self.chonkLevel.text = chonkText
+                    }
+                    
+                }
+            } catch let error {
+                print("Json error")
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+
+    }
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / image.size.width
+        let heightRatio = targetSize.height / image.size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+    func setupLivePreview() {
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        videoPreviewLayer.videoGravity = .resizeAspect
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+        PreviewImage.layer.addSublayer(videoPreviewLayer)
+        
+        DispatchQueue.global(qos: .userInitiated).async { //[weak self] in
+                self.captureSession.startRunning()
+                DispatchQueue.main.async {
+                    self.videoPreviewLayer.frame = self.PreviewImage.bounds
+                }
+            }
+    }
+    var captureSession: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .medium
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+            else {
+                print("Unable to access back camera!")
+                return
+        }
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            stillImageOutput = AVCapturePhotoOutput()
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addInput(input)
+                captureSession.addOutput(stillImageOutput)
+                setupLivePreview()
+            }
+        }
+        catch let error  {
+            print("Error Unable to initialize back camera:  \(error.localizedDescription)")
+        }
+        
+        
+    }
+    @IBOutlet weak var PreviewImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
-
-
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        DispatchQueue.main.async {
+            self.chonkLevel.text = "Scanning..."
+        }
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        stillImageOutput.capturePhoto(with: settings, delegate: self)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.captureSession.stopRunning()
+    }
 }
 
